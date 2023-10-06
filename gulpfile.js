cf856630e6cd3f6c2c2172f60ff49a372ad63137
Bruno Jose Silva <brunojs@gmail.com>
2023-10-06 14:25:35 -0300

    Criando o Hero e fontes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); /* envia o arquivo para a pasta destino */
 }
 
-exports.default = styles; /* tarefa default do npm run build */
+function images() {
+    return gulp.src('./src/images/**/*') 
+        .pipe(imagemin()) /* comprime os arquivos imagens */
+        .pipe(gulp.dest('./dist/images')); /* envia o arquivo para a pasta destino */
+}
+
+exports.default = gulp.parallel(styles); /* tarefa default do npm run build */
+
 exports.watch = function() { /* faz com que o gulp fique olhando para qualquer alteração */
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 }
